c553f8f1b3dd3f200f88c9d50099192d63e408bc
Weining Liu <liuweining@gmail.com>
2014-02-22 14:38:41 -0500

    add script for jacoco-core-snapshots-TM

---
+++ scripts/jacoco-core-snapshots-TM/scripts/wliu.runall.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -xv
+TESTSUBJECT="jacoco-core-snapshots-TM"
+TESTSUBJECT_ALT="jacoco_core"
+SVNLOCAL="$experiment_root/jacoco_snapshots"
+
+for VER in {0..8..1}
+do
+
+cd $SVNLOCAL/v$VER/org.jacoco.core.test
+$experiment_root/$TESTSUBJECT/scripts/TestScripts/scriptR${VER}coverage.cls > $experiment_root/$TESTSUBJECT/outputs/runv${VER}.log 2>&1
+mkdir $experiment_root/$TESTSUBJECT/outputs/v$VER
+mv $experiment_root/$TESTSUBJECT/outputs/t* $experiment_root/$TESTSUBJECT/outputs/v$VER
+done
+
+
